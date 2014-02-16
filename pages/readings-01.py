11d7ffedba54f695ae9ec4d5dff67f7c5eb2de0a
Greg Wilson <gvwilson@third-bit.com>
2014-02-16 08:17:08 -0500

    Reorganizing material to put novice + intermediate at top level

---
+++ pages/readings-01.py
@@ -0,0 +1,9 @@
+import sys
+import numpy as np
+
+def main():
+    script = sys.argv[0]
+    filename = sys.argv[1]
+    data = np.loadtxt(filename, delimiter=',')
+    for m in data.mean(axis=1):
+        print m
