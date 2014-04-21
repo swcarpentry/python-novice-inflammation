b74e6a84ab303a148a6f4185e37053114f5ea83c
Greg Wilson <gvwilson@third-bit.com>
2014-04-21 09:59:41 -0400

    Moving Python climate data example into 'extras' directory

--- pages/07-climate/cstringio-demo.py
+++
@@ -1,6 +0,0 @@
-import cStringIO
-
-data = 'first\nsecond\nthird\n'
-reader = cStringIO.StringIO(data)
-for line in reader:
-    print line
--- pages/07-climate/csv-demo.py
+++
@@ -1,8 +0,0 @@
-import cStringIO
-import csv
-
-data = 'first\nsecond\nthird\n'
-reader = cStringIO.StringIO(data)
-wrapper = csv.reader(reader)
-for record in wrapper:
-    print record
