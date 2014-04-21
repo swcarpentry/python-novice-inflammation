521d5d9ff1b1a1e01e294027ebbc85f7b90e34a5
Greg Wilson <gvwilson@third-bit.com>
2014-04-21 09:59:41 -0400

    Moving Python climate data example into 'extras' directory

--- pages/07-climate/csv-demo-2.py
+++
@@ -1,8 +0,0 @@
-import cStringIO
-import csv
-
-data = '1901,12.3\n1902,45.6\n1903,78.9\n'
-reader = cStringIO.StringIO(data)
-wrapper = csv.reader(reader)
-for record in wrapper:
-    print record
--- pages/07-climate/get-data.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/CAN.csv'
-response = requests.get(url)
-if response.status_code != 200:
-    print 'Failed to get data:', response.status_code
-else:
-    print response.text
