32e128fc195049a7b1d7986113857ae1c8ae0c1f
Pauline Barmby <pbarmby@uwo.ca>
2015-08-02 16:43:57 -0700

    working solution to command-line arithmetic

--- arith.py
+++ arith.py
@@ -2,29 +2,28 @@ import sys
 import numpy
 
 def main():
-    print sys.argv
     assert len(sys.argv) == 4, 'Need exactly 3 arguments'
+
+    operator = sys.argv[1]
+    assert operator in ['add', 'subtract', 'multiply', 'divide'], \
+           'Operator is not one of add, subtract, multiply, or divide: bailing out' 
     try:
-        operand1, operand2 = float(sys.argv[1]), float(sys.argv[3])
+        operand1, operand2 = float(sys.argv[2]), float(sys.argv[3])
     except ValueError:
         print 'cannot convert input to a number: bailing out'
         return
         
-    operator = sys.argv[2]
-    # currently this works with + and - but fails with * and / (either in quotes or not)
-    assert operator in ['+', '-', '*' '/'], \
-           'Operator is not one of +, -, * or /: bailing out' 
     do_arithmetic(operand1, operator, operand2)
 
 def do_arithmetic(operand1, operator, operand2):
 
-    if operator == '+':
+    if operator == 'add':
         value = operand1 + operand2
-    elif operator == '-':
+    elif operator == 'subtract':
         value = operand1 - operand2
-    elif operator == '*':
+    elif operator == 'multiply':
         value = operand1 * operand2
-    elif operator == '/':
+    elif operator == 'divide':
         value = operand1 / operand2
     print value
 
