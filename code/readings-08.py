25778bfffc771aead63bb1085a9af44276b6fc3a
Pauline Barmby <pbarmby@uwo.ca>
2015-08-02 17:08:47 -0700

    readings-08 modifies readings-06 for adding a help message challenge

---
+++ code/readings-08.py
@@ -0,0 +1,36 @@
+import sys
+import numpy
+
+def main():
+    script = sys.argv[0]
+    if len(sys.argv) == 1: # no arguments, so print help message
+        print 'Usage: python readings-08.py action filenames\n \
+               action must be one of --min --mean --max\n \
+               if filenames is blank, input is taken from stdin;\n \
+               otherwise, each filename in the list of arguments is processed in turn'
+        return
+
+    action = sys.argv[1]
+    filenames = sys.argv[2:]
+    assert action in ['--min', '--mean', '--max'], \
+           'Action is not one of --min, --mean, or --max: ' + action
+    if len(filenames) == 0:
+        process(sys.stdin, action)
+    else:
+        for f in filenames:
+            process(f, action)
+
+def process(filename, action):
+    data = numpy.loadtxt(filename, delimiter=',')
+
+    if action == '--min':
+        values = data.min(axis=1)
+    elif action == '--mean':
+        values = data.mean(axis=1)
+    elif action == '--max':
+        values = data.max(axis=1)
+
+    for m in values:
+        print m
+
+main()
