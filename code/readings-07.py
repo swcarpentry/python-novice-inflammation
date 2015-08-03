365131ad3d8813bd431b6235e7382ba44e0db9f2
Pauline Barmby <pbarmby@uwo.ca>
2015-08-02 17:00:28 -0700

    readings-07 modifies readings-06 for changing flags challenge

---
+++ code/readings-07.py
@@ -0,0 +1,29 @@
+import sys
+import numpy
+
+def main():
+    script = sys.argv[0]
+    action = sys.argv[1]
+    filenames = sys.argv[2:]
+    assert action in ['-n', '-m', '-x'], \
+           'Action is not one of -n, -m, or -x: ' + action
+    if len(filenames) == 0:
+        process(sys.stdin, action)
+    else:
+        for f in filenames:
+            process(f, action)
+
+def process(filename, action):
+    data = numpy.loadtxt(filename, delimiter=',')
+
+    if action == '-n':
+        values = data.min(axis=1)
+    elif action == '-m':
+        values = data.mean(axis=1)
+    elif action == '-x':
+        values = data.max(axis=1)
+
+    for m in values:
+        print m
+
+main()
