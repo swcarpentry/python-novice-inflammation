ee07ff37fb53bc00031fb1e3877b49bde5a093af
Pete Bachant <petebachant@gmail.com>
2015-09-11 23:07:45 -0400

    Run PEP8 check from Python script

--- tools/check_pep8.py
+++ tools/check_pep8.py
@@ -7,18 +7,18 @@ import os
 import shutil
 import subprocess
 
-
 def comment_line(line):
     """Comment a line."""
     return "# " + line
 
 
-def md2py(fpath_md, fname_out=None):
+def md2py(fpath_md):
     """
     Convert Markdown file to Python, commenting everything except code blocks.
-    Returns text unless an ouput file name is provided.
+    Output file is in the same directory with `.md` replacing `.py`.
     """
     py_text = ""
+    fpath_py = fpath_md.replace(".md", ".py")
     inblock = False
     with open(fpath_md) as f:
         for line in f:
@@ -32,51 +32,43 @@ def md2py(fpath_md, fname_out=None):
                 py_text += line
             else:
                 py_text += comment_line(line)
-    if fname_out is None:
-        return py_text
-    else:
-        if not os.path.isdir("temp"):
-            os.mkdir("temp")
-        with open(os.path.join("temp", fname_out), "w") as f:
-            f.write(py_text)
+    with open(fpath_py, "w") as f:
+        f.write(py_text)
 
 
-def test_md2py(rm_temp=True):
+def test_md2py(rm=True):
     """Test the md2py function on known input."""
     fpath_md = "tools/test/test_md_to_py.md"
-    fpath_py_ref = "tools/test/test_md_to_py.py"
-    fname_py = "test.py"
-    md2py(fpath_md, fname_py)
+    fpath_py_ref = "tools/test/test_md_to_py_ref.py"
+    fpath_py = "tools/test/test_md_to_py.py"
+    md2py(fpath_md)
     with open(fpath_py_ref) as f:
         py_ref = f.read()
-    with open(os.path.join("temp", fname_py)) as f:
+    with open(fpath_py) as f:
         py = f.read()
-    if rm_temp:
-        shutil.rmtree("./temp")
+    if rm:
+        os.remove(fpath_py)
     assert py == py_ref
 
 
-def check_lesson(fname):
+def check_page(fpath, rm=True, ignore=["E501", "W291", "E402", "E302"]):
     """
-    Convert a lesson and check it for PEP8 compliance. Does not work right now.
+    Convert a lesson page from Markdown to Python and check it for PEP8
+    compliance.
     """
-    fname_py = fname.replace(".md", ".py")
-    fpath_py = os.path.join("temp", fname_py)
+    fpath_py = fpath.replace(".md", ".py")
     # Convert the lesson to Python
-    md2py(fname, fname_py)
+    md2py(fpath)
     # Run the pep8 script on the newly generated Python file
-    results = subprocess.check_output(["pep8", fpath_py], shell=True,
-                                      stderr=subprocess.STDOUT)
-    print(results)
+    subprocess.call(["pep8", fpath_py, "--ignore", ",".join(ignore)])
+    if rm:
+        os.remove(fpath_py)
 
 
 if __name__ == "__main__":
-    # test_md2py(rm_temp=True)
-    # check_lesson("01-numpy.md")
+    # test_md2py(rm=True)
     pages = ["01-numpy.md", "02-loop.md", "03-lists.md", "04-files.md",
              "05-cond.md", "06-func.md", "07-errors.md", "08-defensive.md",
              "09-debugging.md", "10-cmdline.md"]
     for page in pages:
-        fname_py = page.replace(".md", ".py")
-        print("Converting {} to {}".format(page, fname_py))
-        md2py(page, fname_py)
+        check_page(page, rm=True)
