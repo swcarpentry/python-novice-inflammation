fdf5ef2fe515297ebcf533ca942ae977ec5cb600
Shannon Osborne <sosborne@stsci.edu>
2019-11-07 14:55:49 -0500

    Change mean method for readings_03.py (#736)

--- code/readings_03.py
+++ code/readings_03.py
@@ -6,7 +6,7 @@ def main():
     script = sys.argv[0]
     for filename in sys.argv[1:]:
         data = numpy.loadtxt(filename, delimiter=',')
-        for m in data.mean(axis=1):
+        for m in numpy.mean(data, axis=1):
             print(m)
 
 
