9a4b3597b4c5f570c8269ab205850a21b5182682
Stephen <36123293+sparce@users.noreply.github.com>
2019-11-11 09:44:22 +1100

    Update loop variable names

--- code/readings_03.py
+++ code/readings_03.py
@@ -6,9 +6,8 @@ def main():
     script = sys.argv[0]
     for filename in sys.argv[1:]:
         data = numpy.loadtxt(filename, delimiter=',')
-        for m in numpy.mean(data, axis=1):
-            print(m)
-
+        for row_mean in data.mean(axis=1):
+            print(row_mean)
 
 if __name__ == '__main__':
     main()
