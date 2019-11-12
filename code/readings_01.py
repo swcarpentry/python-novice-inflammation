a5aed80fa038320b34088ddf93a20cde88d42e06
Stephen Pearce <36123293+sparce@users.noreply.github.com>
2019-11-13 02:42:23 +1100

    code/readings_01.py: harmonise with lesson text (#744)

--- code/readings_01.py
+++ code/readings_01.py
@@ -6,5 +6,5 @@ def main():
     script = sys.argv[0]
     filename = sys.argv[1]
     data = numpy.loadtxt(filename, delimiter=',')
-    for m in data.mean(axis=1):
-        print(m)
+    for row_mean in numpy.mean(data, axis=1):
+        print(row_mean)
