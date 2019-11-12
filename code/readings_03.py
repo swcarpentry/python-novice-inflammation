427265359d91a80909efbc14aa92faf167f9e6b0
Stephen <36123293+sparce@users.noreply.github.com>
2019-11-12 14:19:59 +1100

    Consistency with lesson text

--- code/readings_03.py
+++ code/readings_03.py
@@ -6,7 +6,7 @@ def main():
     script = sys.argv[0]
     for filename in sys.argv[1:]:
         data = numpy.loadtxt(filename, delimiter=',')
-        for row_mean in data.mean(axis=1):
+        for row_mean in numpy.mean(data, axis=1):
             print(row_mean)
 
 if __name__ == '__main__':
