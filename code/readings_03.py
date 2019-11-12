1cec7dc59cd681ee6e243b0eda028930b6794241
Stephen Pearce <36123293+sparce@users.noreply.github.com>
2019-11-13 02:48:57 +1100

    code/readings_03.py: Consistency with lesson text (#746)

--- code/readings_03.py
+++ code/readings_03.py
@@ -6,8 +6,8 @@ def main():
     script = sys.argv[0]
     for filename in sys.argv[1:]:
         data = numpy.loadtxt(filename, delimiter=',')
-        for m in numpy.mean(data, axis=1):
-            print(m)
+        for row_mean in numpy.mean(data, axis=1):
+            print(row_mean)
 
 
 if __name__ == '__main__':
