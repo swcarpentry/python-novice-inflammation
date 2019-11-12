79df0eff6e4831ab27eeaa6ac2b23eddb6b3efdf
Stephen Pearce <36123293+sparce@users.noreply.github.com>
2019-11-13 02:55:35 +1100

    code/readings_05.py: Harmonise with lesson text (#748)

--- code/readings_05.py
+++ code/readings_05.py
@@ -1,30 +1,27 @@
 import sys
 import numpy
 
-
 def main():
     script = sys.argv[0]
     action = sys.argv[1]
     filenames = sys.argv[2:]
-    assert action in ['--min', '--mean', '--max'], (
-        'Action is not one of --min, --mean, or --max: ' + action)
-    for f in filenames:
-        process(f, action)
-
+    assert action in ['--min', '--mean', '--max'], \
+           'Action is not one of --min, --mean, or --max: ' + action
+    for filename in filenames:
+        process(filename, action)
 
 def process(filename, action):
     data = numpy.loadtxt(filename, delimiter=',')
 
     if action == '--min':
-        values = data.min(axis=1)
+        values = numpy.min(data, axis=1)
     elif action == '--mean':
-        values = data.mean(axis=1)
+        values = numpy.mean(data, axis=1)
     elif action == '--max':
-        values = data.max(axis=1)
+        values = numpy.max(data, axis=1)
 
-    for m in values:
-        print(m)
-
+    for val in values:
+        print(val)
 
 if __name__ == '__main__':
-    main()
+   main()
