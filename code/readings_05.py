fb0c680958ad36415c41748cd839150fd836c240
Stephen <36123293+sparce@users.noreply.github.com>
2019-11-12 14:28:56 +1100

    Harmonise with lesson text

--- code/readings_05.py
+++ code/readings_05.py
@@ -14,11 +14,11 @@ def process(filename, action):
     data = numpy.loadtxt(filename, delimiter=',')
 
     if action == '--min':
-        values = data.min(axis=1)
+        values = numpy.min(data, axis=1)
     elif action == '--mean':
-        values = data.mean(axis=1)
+        values = numpy.mean(data, axis=1)
     elif action == '--max':
-        values = data.max(axis=1)
+        values = numpy.max(data, axis=1)
 
     for val in values:
         print(val)
